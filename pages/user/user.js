75fbbbf2fa651cef530839614305807af0cf56ce
侯俊宇 <260470264@qq.com>
2024-03-13 10:00:30 +0800

    个人中心
    个人中心模块进一步完善

--- pages/user/user.js
+++ pages/user/user.js
@@ -1,18 +1,60 @@
 // pages/user/user.js
+const app = getApp();
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
+    userInfo: {},
+  },
 
+  /**
+   * 获取用户信息
+   */
+  getInfo() {
+    wx.getUserProfile({
+      desc: '用于完善会员资料',
+      success: (res) => {
+        let { avatarUrl, nickName, gender, country, province, city } = res.userInfo;
+        wx.request({
+          url: 'http://localhost:8080/mini/update/info',
+          method: 'POST',
+          header: {
+            'Authorization': wx.getStorageSync('token'),
+          },
+          data: {
+            nickName: nickName,
+            avatar: avatarUrl,
+            openId: wx.getStorageSync('openid'),
+          },
+          success: (res) => {
+            console.log(res)
+          },
+          fail: (err) => {
+            console.log("接口请求失败：--->", err)
+          }
+        });
+      }
+    })
+  },
+
+  /**
+   * 退出登录
+   */
+  logout() {
+    wx.clearStorageSync();
+    wx.exitMiniProgram();
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad(options) {
-
+    this.setData({
+      userInfo: wx.getStorageSync('userInfo')
+    });
   },
 
   /**
